fcc6e3e0e6e058b3c8edab53f5a4ad7e167b471b
Blannon <ngogeblannon10@gmail.com>
2023-12-24 17:35:33 +0300

    update with animation and button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    //Lottie
+    implementation( "com.airbnb.android:lottie-compose:6.2.0")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/blannon_network/lottieanimation/MainActivity.kt
+++ app/src/main/java/com/blannon_network/lottieanimation/MainActivity.kt
@@ -3,13 +3,27 @@ package com.blannon_network.lottieanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.blannon_network.lottieanimation.ui.theme.LottieAnimationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WelcomeScreen()
                 }
             }
         }
@@ -30,17 +44,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LottieAnimationTheme {
-        Greeting("Android")
+fun WelcomeScreen(){
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.animation))
+    var isPlaying by remember{
+        mutableStateOf(true)
     }
+    val progress by animateLottieCompositionAsState(composition = composition,
+        isPlaying= isPlaying)
+
+    LaunchedEffect(key1 = progress){
+        if (progress == 0f)
+            isPlaying = true
+    }
+    if (progress == 1f)
+        isPlaying = false
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center, 
+        horizontalAlignment = Alignment.CenterHorizontally 
+    ){
+        LottieAnimation(
+            modifier = Modifier.size(250.dp),
+            composition = composition,
+        progress = {progress})
+        Button(onClick = {isPlaying = true}) {
+            Text(text = "Add to Cart")
+            
+        }
+        
+    }
+
 }
